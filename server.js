496b03e1b851db58a43173ea073a050ae700cc3a
John-Mike Marquardt <jmmarquardt@gmail.com>
2016-11-29 14:42:03 -0500

    switching to jade for templating

--- server.js
+++ server.js
@@ -26,13 +26,13 @@ app.use(bodyParser.urlencoded({
 // override with POST having ?_method=DELETE
 app.use(methodOverride('_method'));
 
-var exphbs = require('express-handlebars');
+// var exphbs = require('express-handlebars');
+//
+// app.engine('handlebars', exphbs({
+// 	defaultLayout: 'main'
+// }));
 
-app.engine('handlebars', exphbs({
-	defaultLayout: 'main'
-}));
-
-app.set('view engine', 'handlebars');
+app.set('view engine', 'pug');
 
 var routes = require('./controllers/burgers_controller.js');
 
